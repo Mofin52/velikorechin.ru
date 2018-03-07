7a5025258a86aff719ada0569241acde1bf4846e
Alexander Velikorechin <velikorechin@uchi.ru>
2018-03-07 22:12:19 +0300

    progress bar ready

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {  
+    var winHeight = $(window).height(), 
+        docHeight = $(document).height(),
+        progressBar = $('progress'),
+        max, value;
+
+    max = docHeight - winHeight;
+    progressBar.attr('max', max);
+  
+    $(document).on('scroll', function(){
+       value = $(window).scrollTop();
+       progressBar.attr('value', value);
+    });
+
+  });
